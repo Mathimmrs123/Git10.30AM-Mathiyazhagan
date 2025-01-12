80574e271f4623b9095c50347ac101745701f1b2
Mathimmrs123 <mathimmrs123@gmail.com>
2025-01-12 11:58:03 +0530

    add employee methods and objects

--- src/test/java/com/git10/ClassA.java
+++ src/test/java/com/git10/ClassA.java
@@ -1,5 +1,26 @@
 package com.git10;
 
 public class ClassA {
+	
+	private void empName() {
+		System.out.println("Monish");
+	
+	}
+	private void empLocation() {
+		System.out.println("Chennai");
+
+	}
+	private void empCompany() {
+		System.out.println("RMK Glass");
+
+	}
+	public static void main(String[] args) {
+		ClassA a=new ClassA();
+		
+		a.empName();
+		a.empLocation();
+		a.empCompany();
+		
+	}
 
 }
